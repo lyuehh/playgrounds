b75f6757d4a231df244bea7601432636d2f06bd7
lyuehh <lyuehh@gmail.com>
2015-04-26 11:37:48 +0800

    update 4_xor.py

--- python/4_xor.py
+++ python/4_xor.py
@@ -1,11 +1,23 @@
 #!/bin/python
 
 # Complete the function below.
+import operator
 
 
-def  maxXor( l,  r):
-    pass
+def  maxXor(l,  r):
+    max = 1
+    if l > r:
+        l, r = r, l
 
+    for i in range(l, r+1):
+        for j in range(l, r+1):
+            # print("%d %d" % (i, j))
+            x = operator.xor(i, j)
+            # print("x1: %s" % x)
+            if x > max:
+                max = x
+
+    return max
 
 
 _l = int(raw_input());
@@ -13,5 +25,9 @@ _r = int(raw_input());
 
 res = maxXor(_l, _r);
 print(res)
+
+# l = range(1, 10)
+# for i, e in enumerate(l):
+#     print i, e
 
 
